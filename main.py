c502e5af3d5a3f0d012c8b37ee165706794dd148
generaldenmark <asger@weirsoe.dk>
2020-12-28 12:08:54 +0100

    Initial bot commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!', description="What")
+
+
+def write_in_channel(str):
+    return {
+        "717074975923634188": {
+            "description": """
+Kære kammerater!
+
+Vi har brug for din støtte! 
+Hver måned er jeg blevet bedt om at sende denne besked ud til jer alle sammen, og pænt minde jer om at betale kontigent.
+
+Hvad i betaler, er op til jer, hvad end i kan udvære.
+
+Overfør pengende til Mobile pay boksen:
+https://mobilepay.dk/box?CWdpCOACfkVtEqLiFALfQxRhA
+
+Skriv jeres navn i kommentarfeltet,
+
+
+og fortsæt den gode kamp!
+
+Kærligst LAG-gruppen
+            """,
+            "footer": 'Denne besked er produceret af en automatisk Rød klient',
+            "title": 'Det er tid til at betale kontigent!',
+            'color': discord.Color.red(),
+            'image': 'https://dkp.dk/wp-content/uploads/2017/09/logohammerogsegl.png',
+            'url': 'https://mobilepay.dk/box?CWdpCOACfkVtEqLiFALfQxRhA'
+        },
+    }.get(str, None)
+
+
+# Events
+@bot.event
+async def on_ready():
+    guild: discord.Guild
+    try:
+        for guild in bot.guilds:
+            channel: discord.TextChannel
+            for channel in guild.channels:
+                if type(channel) == discord.TextChannel:
+                    msg = write_in_channel(str(channel.id))
+                    if msg is not None:
+                        if type(msg) == str:
+                            await channel.send(msg)
+                        else:
+                            embed = discord.Embed(**msg)
+                            if msg.get('image', None) is not None:
+                                embed.set_image(url=msg.get('image'))
+                            if msg.get('footer', None) is not None:
+                                embed.set_footer(text=msg.get('footer'))
+                            await channel.send(embed=embed)
+    finally:
+        await bot.logout()
+
+bot.run('NzkzMDY2OTA0ODM4NDcxNzIw.X-m3HA._lZjuwj_DbXt0dICQCbERUEea38')
